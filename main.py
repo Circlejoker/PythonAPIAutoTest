c43617b2f240401c0feb0c5c9cdf4f478625d6ec
Circlejoker <767768717@qq.com>
2022-07-19 12:40:22 +0800

    更新代码

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # 使用pytest收集所有的用例并运行，输出allure报告
-import os
 import pytest
-from Common.my_path import report_dir
+
 
 if __name__ == '__main__':
-    pytest.main(['-s','-v','./testcase',"allure={}".format(report_dir)])
+    pytest.main(['-s','-v','./testcase','--alluredir=outputs/reports'])
 
 
 
